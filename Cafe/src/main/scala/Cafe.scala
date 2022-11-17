113bdd755a1e33edbb46de5e8394bfcd5593c077
April Garlington <april.garlington@hotmail.co.uk>
2022-11-17 11:53:03 +0000

    loyalty card and premium items method

--- Cafe/src/main/scala/Cafe.scala
+++ Cafe/src/main/scala/Cafe.scala
@@ -6,38 +6,44 @@ object Cafe {
     cafe.copy(menuItems = cafe.menuItems :+ menuItem)
   }
 
-  def totalBill(cafe: Cafe, customer: Customer): BigDecimal = {
-    val bill = cafe.order.map(item => item.price).sum
-    if (customer.loyaltyCard && customer.numberOfStars >= 3 && customer.numberOfStars <= 8){
-       customer.numberOfStars * (bill*0.025)
+  def loyaltyDiscount(cafe: Cafe, customer: Customer): BigDecimal = {
+    val premiumItemsOrdered = {customer.order.filter { item => item.Premium }}
+    val premiumTotalPrice = {premiumItemsOrdered.map(item => item.price).sum}
+    if(customer.order.exists(item => item.Premium) && customer.loyaltyCard && customer.numberOfStars >= 3 && customer.numberOfStars <= 8) {
+      (customer.numberOfStars * ((totalBill(cafe)-premiumTotalPrice) * 0.025)) + premiumTotalPrice
+    } else if (customer.loyaltyCard && customer.numberOfStars >= 3 && customer.numberOfStars <= 8) {
+        customer.numberOfStars * (totalBill(cafe) * 0.025)
     } else {
-      bill
+        totalBill(cafe)
     }
-
   }
 
-  def addVatToBill(cafe: Cafe, customer: Customer): BigDecimal = {
-    val serviceCharge10 = totalBill(cafe, customer)*0.1
-    val serviceCharge20 = totalBill(cafe, customer)*0.2
-    val serviceCharge25 = totalBill(cafe, customer)*0.25
+  def totalBill(cafe: Cafe): BigDecimal = {
+    cafe.order.map(item => item.price).sum
+  }
+
+  def addServiceToBill(cafe: Cafe, customer: Customer): BigDecimal = {
+    val serviceCharge10 = totalBill(cafe)*0.1
+    val serviceCharge20 = totalBill(cafe)*0.2
+    val serviceCharge25 = totalBill(cafe)*0.25
 
    if (cafe.order.exists(item => item.Premium)) {
      if (serviceCharge25 <= 40.00) {
-       totalBill(cafe, customer) + serviceCharge25
+       totalBill(cafe) + serviceCharge25
      } else {
-       totalBill(cafe, customer) + 40.00
+       totalBill(cafe) + 40.00
      }
    } else if (cafe.order.exists(item => item.Hot && !item.Drink)) {
       if (serviceCharge20 <= 20.00){
-        totalBill(cafe, customer) + serviceCharge20
+        totalBill(cafe) + serviceCharge20
       } else {
-        totalBill(cafe, customer) + 20.00
+        totalBill(cafe) + 20.00
       }
     } else if (cafe.order.exists(item => !item.Hot && !item.Drink)) {
-      totalBill(cafe, customer) + serviceCharge10
+      totalBill(cafe) + serviceCharge10
     }
     else {
-      totalBill(cafe, customer)
+      totalBill(cafe)
     }
   }
 
